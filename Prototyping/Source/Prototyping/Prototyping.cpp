38bf1a1e5e80fc113876309606fa17a000c2af39
dev <devin.ray.broussard@gmail.com>
2023-01-27 16:02:05 -0600

    new project

--- Prototyping/Source/Prototyping/Prototyping.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Prototyping.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Prototyping, "Prototyping");
-
-DEFINE_LOG_CATEGORY(LogFlying)
